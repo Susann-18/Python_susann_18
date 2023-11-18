1a655d93661af157f5d41ab03d54387adb938710
Susann-18 <151371561+Susann-18@users.noreply.github.com>
2023-11-18 17:04:36 +0530

    Add files via upload

---
+++ python pgm/assignments.py
@@ -0,0 +1,25 @@
+#check whether leap year or not
+#  year=(int(input("Enter  any the  checked is for leap:")))
+# if(year % 4)==0:
+#     if(year % 100)==0:
+#         if(year % 400)==0:
+#             print("the  is  given year leap")
+#         else:
+#             print("rhe is not year leap")
+#     else:
+#         print("the is not year leap")
+# else:
+#     print("the is not year leap")
+# positive or negative
+# num=(int(input("Enter the number:")))
+# if num>=0:
+#     print("postive")
+# else:
+#     print("negative")
+# odd or even
+num=(int(input("Enter the odd or even number:")))
+if (num%2)==0:
+    print("the odd number")
+else:
+    print("the even number")
+
