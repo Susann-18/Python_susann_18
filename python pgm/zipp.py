be80655e5624611d35f9268baf113253997163a8
Susann-18 <151371561+Susann-18@users.noreply.github.com>
2023-12-08 16:11:56 +0530

    Add files via upload

--- python pgm/zipp.py
+++ python pgm/zipp.py
@@ -14,7 +14,29 @@
 #     zipf.write("test1.txt")
 #     zipf.write("test2.csv")
 
+# import zipfile
+# with zipfile.ZipFile("test.zip","r")as zipfile:
+#     file_list=zipfile.namelist()
+#     print("content of the zipfile",file_list)
+
+# import zipfile
+# with zipfile.ZipFile("datatest.zip","w")as zipfile:
+#     zipfile.write("zipffile.txt")
+#     zipfile.write("zipffilee.txt")
+
+# import zipfile
+# with zipfile.ZipFile("datatest.zip","r")as zipfile:
+#     file_list=zipfile.namelist()
+#     print("content of the zipfile",file_list)
+
 import zipfile
-with zipfile.ZipFile("test.zip","r")as zipfile:
-    file_list=zipfile.namelist()
-    print("content of the zipfile",file_list)
+
+file_to_extract = 'zipffile.txt'
+extraction_path = "C:/Users/Cyber Security/Music"
+
+with zipfile.ZipFile('datatest.zip', 'r') as zipf:
+    if file_to_extract in zipf.namelist():
+        zipf.extract(file_to_extract, extraction_path)
+        print(f"'{file_to_extract}' has been extracted to '{extraction_path}'.")
+    else:
+        print(f"'{file_to_extract}' does not exist in the ZIP archive.")
