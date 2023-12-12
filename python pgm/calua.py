2e6530c62a4a9aa83f4217207d075610bf828df8
Susann-18 <151371561+Susann-18@users.noreply.github.com>
2023-12-12 17:33:41 +0530

    Add files via upload

---
+++ python pgm/calua.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+root=Tk()
+root.geometry('250x300')
+root.title("Simple calculatro")
+root.configure(bg='sky blue')
+frame=Frame(root,text=0,bg="black",fg="white")
+frame.grid(row=0,column=0)
+# field=Entry(frame,font="arial")
+# lb1=Label(root)
+root.mainloop()
+
+        
