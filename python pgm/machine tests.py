e6f936117ae73cac752d091106a177c96bb6481e
Susann-18 <151371561+Susann-18@users.noreply.github.com>
2023-12-19 14:29:14 +0530

    Add files via upload

---
+++ python pgm/machine tests.py
@@ -0,0 +1,63 @@
+# 1.odd or even
+
+# x=int(input("Enter a number:"))
+# if x%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# 2. 
+# class Car:
+#     def __init__(self,make,model,year):
+#         self.make=make
+#         self.model=model
+#         self.year=year
+#     def display_info(self):
+#         print(f"{self.year},{self.make},{self.model}")
+# my_car=Car("Volkvagan","Polo",2018)
+# my_car.display_info()
+
+
+# 3.
+# import tkinter as tk
+# from tkinter import messagebox
+
+# def register():
+#     username = entry_username.get()
+#     password = entry_password.get()
+#     if username and password:
+#         messagebox.showinfo("Registration Successful", "You have been registered successfully")
+#     else:
+#         messagebox.showinfo("Error", "Please fill in all fields.")
+
+# root = tk.Tk()
+# root.title("Registration Form")
+# label_username = tk.Label(root, text="Username")
+# label_username.place(x=100,y=50)
+# entry_username = tk.Entry(root)
+# entry_username.place(x=100,y=100)
+
+# label_password = tk.Label(root, text="Password")
+# label_password.place(x=100,y=150)
+# entry_password = tk.Entry(root)
+# entry_password.place(x=100,y=200)
+
+# button = tk.Button(root, text="Register", command=register)
+# button.place(x=100,y=300)
+
+# root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
